4adf93b51112d66cfcbca1f643354b9ec20c9d23
plony benav <menachemfuterfas@gmail.com>
2020-11-17 15:46:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
           :colors => ["Black", "White"],
           :players => [{:player_name => 'Alan Anderson',
                         :number => 0,
-                        :shoe => 15,
+                        :shoe => 16,
                         :points => 22,
                         :rebounds => 12, 
                         :assists => 12,
