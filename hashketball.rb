3a1aeff8adade99ff7cdc371ae5cd83c9163a89f
plony benav <menachemfuterfas@gmail.com>
2020-11-17 15:43:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,17 +1,57 @@
 # Write your code here!
 def game_hash
-{
+game_hash = {
 :home => {:team_name => "Brooklyn Nets",
           :colors => ["Black", "White"],
-          :players => [{:player_name => 0,
+          :players => [{:player_name => 'Alan Anderson',
                         :number => 0,
-                        :shoe => 0,
-                        :points => 0,
-                        :rebounds => 0, 
-                        :assists => 0,
-                        :steals => 0,
-                        :blocks => 0,
-                        :slam_dunks => 0}]},
+                        :shoe => 15,
+                        :points => 22,
+                        :rebounds => 12, 
+                        :assists => 12,
+                        :steals => 3,
+                        :blocks => 1,
+                        :slam_dunks => 1},
+                        
+                        {:player_name => 'Reggie Evans',
+                            :number => 30,
+                            :shoe => 14,
+                            :points => 12,
+                            :rebounds => 12, 
+                            :assists => 12,
+                            :steals => 12,
+                            :blocks => 12,
+                            :slam_dunks => 7},
+
+                        {:player_name => 'Brook Lopez',
+                            :number => 11,
+                            :shoe => 17,
+                            :points => 17,
+                            :rebounds => 19, 
+                            :assists => 10,
+                            :steals => 3,
+                            :blocks => 1,
+                            :slam_dunks => 15},
+
+                        {:player_name => 'Mason Plumlee',
+                            :number => 1,
+                            :shoe => 19,
+                            :points => 26,
+                            :rebounds => 11, 
+                            :assists => 6,
+                            :steals => 3,
+                            :blocks => 8,
+                            :slam_dunks => 5},
+                            
+                        {:player_name => 'Jason Terry',
+                            :number => 31,
+                            :shoe => 15,
+                            :points => 29,
+                            :rebounds => 2, 
+                            :assists => 2,
+                            :steals => 4,
+                            :blocks => 11,
+                            :slam_dunks => 1}]},
 
 
 :away => {:team_name => "Charlotte Hornets",
@@ -26,4 +66,5 @@ def game_hash
                         :blocks => 0,
                         :slam_dunks => 0}]}
 }
+
 end
