a64350c3ebdfce2cfa000c9faeff4ba2173aa5e9
plony benav <menachemfuterfas@gmail.com>
2020-11-17 15:29:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,29 @@
 # Write your code here!
 def game_hash
-{}  
+{
+:home => {:team_name => "Brooklyn Nets",
+          :colors => ["Black", "White"],
+          :players => [{:player_name => 0,
+                        :number => 0,
+                        :shoe => 0,
+                        :points => 0,
+                        :rebounds => 0, 
+                        :assists => 0,
+                        :steals => 0,
+                        :blocks => 0,
+                        :slam_dunks => 0}]},
+
+
+:away => {:team_name => "Charlotte Hornets",
+          :colors => ["Turquoise", "Purple"],
+          :players => [{:player_name => 0,
+                        :number => 0,
+                        :shoe => 0,
+                        :points => 0,
+                        :rebounds => 0, 
+                        :assists => 0,
+                        :steals => 0,
+                        :blocks => 0,
+                        :slam_dunks => 0}]}
+}
 end
